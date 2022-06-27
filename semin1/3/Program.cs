fdb75154c52019568e9ac64cd3268ee8bed0b96d
Natasha <lickevich.natasha@rambler.ru>
2022-06-27 23:41:42 +0300

    задача3

---
+++ semin1/3/Program.cs
@@ -0,0 +1,16 @@
+﻿{
+  int a;
+  Console.WriteLine("введите число");
+  a = int.Parse(Console.ReadLine());
+  if (a % 2 == 0)
+  {
+     Console.Write("Введенное число является четным");
+     Console.Read();
+  }
+   else
+  {
+      Console.Write("Введенное число нечетное");
+      Console.Read();
+  }
+}
+
