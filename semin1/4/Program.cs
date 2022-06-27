0a5a7cab5ee2af709df464f69a29f2320ef400f9
Natasha <lickevich.natasha@rambler.ru>
2022-06-27 23:44:20 +0300

    задача4

---
+++ semin1/4/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+  Console.WriteLine("введите число");
+  int n=Convert.ToInt32(Console.ReadLine());
+  int a=1;
+  for (a=1;a<=n;a++)
+  {
+    if ( a % 2 ==0)
+
+    {
+        Console.WriteLine( a + "");
+ 
+    }
+}
+}
+
